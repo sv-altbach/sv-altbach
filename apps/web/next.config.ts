04c5e18386ed0ae4fee24bf8324de62defe27f27
Deniz Gazitepe <dg102@hdm-stuttgart.de>
2025-12-13 17:54:19 +0100

    feat(web): subdomain rewrites

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,8 +1,30 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
+const ROOT_DOMAIN = process.env.VERCEL_ENV
+	? `(${process.env.VERCEL_URL}|${process.env.VERCEL_BRANCH_URL}|${process.env.VERCEL_PROJECT_PRODUCTION_URL})`
+	: "localhost";
+
+const REWRITE_SOURCE_PATTERN =
+	"/:path((?!_next|_vercel|.well-known|.*\\.\\w+$).*)*";
+
+export default {
 	typedRoutes: true,
 	reactCompiler: true,
-};
 
-export default nextConfig;
+	rewrites: () => ({
+		beforeFiles: [
+			// * route subdomain requests to /sub/:subdomain/:path*
+			{
+				source: REWRITE_SOURCE_PATTERN,
+				has: [{ type: "host", value: `(?<subdomain>.*).${ROOT_DOMAIN}` }],
+				destination: "/sub/:subdomain/:path*",
+			},
+			// * route root domain requests to /root/:path*
+			{
+				source: REWRITE_SOURCE_PATTERN,
+				has: [{ type: "host", value: ROOT_DOMAIN }],
+				destination: "/root/:path*",
+			},
+		],
+	}),
+} satisfies NextConfig;
---
+++ apps/web/src/app/root/page.tsx
@@ -0,0 +1,3 @@
+export default function Root() {
+	return <div>Work in progress</div>;
+}
