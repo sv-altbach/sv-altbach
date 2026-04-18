247fceff36bd19ce621c9e901157cad7eb1fd4e7
Alexander Muzenhardt <alexander.muzenhardt@cit.de>
2026-04-18 12:05:32 +0200

    feat(news): Aktualisieren der Neuigkeiten für die dritte Saison

---
+++ apps/web/src/app/sub/masters/new-scoring-system/page.tsx
@@ -0,0 +1,12 @@
+import {Box, Heading, Text} from "@radix-ui/themes";
+
+export default function NewScoringSystemPage() {
+    return (
+        <main>
+            <Box p="9">
+                <Heading as="h1">Neues Punktesystem</Heading>
+                <Text as="p">Weitere Informationen folgen in Kürze.</Text>
+            </Box>
+        </main>
+    )
+}
