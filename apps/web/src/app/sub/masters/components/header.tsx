ccac83f75e1433f1d06bb3a5da19b8b322f21e18
Alex Muzenhardt <alexander.muzenhardt@gmail.com>
2026-04-18 16:40:11 +0200

    chore: Ändern des Buttons auf Radix-BUtton im Kopfbereich

--- apps/web/src/app/sub/masters/components/header.tsx
+++ apps/web/src/app/sub/masters/components/header.tsx
@@ -1,30 +1,30 @@
-import { Flex, Heading, Text } from "@radix-ui/themes";
-import { IconChessKnightFilled } from "@tabler/icons-react";
+import {Flex, Heading, Text, Button} from "@radix-ui/themes";
+import {IconChessKnightFilled} from "@tabler/icons-react";
 import Link from "next/link";
-import { Button } from "@/components/ui/button";
 
 export function Header() {
-	return (
-		<>
-			<IconChessKnightFilled className="mb-5 text-5xl text-white" />
+    return (
+        <>
+            <IconChessKnightFilled className="mb-5 text-5xl text-white"/>
 
-			<Heading as="h1" className="text-white" size="9">
-				SVA <Text className="text-red-600">Masters</Text>
-			</Heading>
+            <Heading as="h1" className="text-white" size="9">
+                SVA <Text className="text-red-600">Masters</Text>
+            </Heading>
 
-			<Text as="p" size="5" className="text-white">
-				29. Mai 2026, 73035 Göppingen
-			</Text>
+            <Text as="p" size="5" className="text-white">
+                29. Mai 2026, 73035 Göppingen
+            </Text>
 
-			<Flex gap="4" mt="5">
-				<Button
-					render={<Link href="/scoreboard" />}
-					nativeButton={false}
-					size="xl"
-				>
-					Zur Rangliste
-				</Button>
-			</Flex>
-		</>
-	);
+            <Flex gap="4" mt="5">
+                <Button
+                    asChild
+                    size="4"
+                >
+                    <Link href="/scoreboard">
+                        Zur Rangliste
+                    </Link>
+                </Button>
+            </Flex>
+        </>
+    );
 }
