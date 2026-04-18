3213d0d42b3a82d9026c515a7fce664e06903806
Alex Muzenhardt <alexander.muzenhardt@gmail.com>
2026-04-18 13:55:28 +0200

    chore(about): Entfernen der Abschnitte "Wo?" und "Wann?"

--- apps/web/src/app/sub/masters/components/about.tsx
+++ apps/web/src/app/sub/masters/components/about.tsx
@@ -5,8 +5,7 @@ import {News} from "@/app/sub/masters/components/news";
 
 export function About() {
     return (
-        <>
-            <Flex
+        <Flex
                 asChild
                 direction={{initial: "column", md: "row"}}
                 gap="8"
@@ -138,74 +137,5 @@ export function About() {
                     </Box>
                 </section>
             </Flex>
-
-            <section>
-                <Heading as="h3" size="6" mb="2">
-                    Wo?
-                </Heading>
-
-                <Grid
-                    columns={{xs: "3"}}
-                    gap={{initial: "6", xs: "3"}}
-                    width="auto"
-                >
-                    <address className="not-italic">
-                        <strong>Sporthalle Altbach</strong> <br/> Esslinger Straße 90{" "}
-                        <br/> 73776 Altbach <br/> Deutschland
-                    </address>
-
-                    <address className="not-italic">
-                        <strong>Haus der Jugend</strong> <br/> Dürerstraße 21 <br/> 73033
-                        Göppingen <br/> Deutschland
-                    </address>
-
-                    <address className="not-italic">
-                        <strong>Tanzschule Plochingen</strong> <br/> Esslinger Straße 14{" "}
-                        <br/> 73207 Plochingen <br/> Deutschland
-                    </address>
-                </Grid>
-            </section>
-
-            <section>
-                <Heading as="h3" size="6" mb="2">
-                    Wann?
-                </Heading>
-
-                <ul>
-                    <li>
-                        <del>
-                            Freitag, 07. Februar 2025 <strong>(Blitz)</strong>
-                        </del>
-                    </li>
-                    <li>
-                        <del>
-                            Freitag, 28. März 2025 <strong>(Blitz)</strong>
-                        </del>
-                    </li>
-                    <li>
-                        <del>
-                            Sonntag, 25. Mai 2025 <strong>(Rapid)</strong>
-                            <em>- in Göppingen</em>
-                        </del>
-                    </li>
-                    <li>
-                        <del>
-                            Samstag, 12. Juli 2025 <strong>(Rapid)</strong>
-                        </del>
-                    </li>
-                    <li>
-                        <del>
-                            Samstag, 29. November 2025 <strong>(960 Random)</strong>
-                            <em>- in Plochingen</em>
-                        </del>
-                    </li>
-                    <li className="mt-5">
-                        <strong>Finale:</strong> 18. April 2026
-                        <br/>
-                        <small>(Einladungsturnier)</small>
-                    </li>
-                </ul>
-            </section>
-        </>
     );
 }
