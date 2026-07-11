1fc774a9d9e0fe997b989b7a2530e05df6438b7d
Alex Muzenhardt <alexander.muzenhardt@gmail.com>
2026-07-11 15:35:52 +0200

    fix(masters/scoreboard): Spieler, die die Mindestvoraussetzung für die Finalteilnahme nicht erfüllt haben, werden korrekt markiert.

--- apps/web/src/app/sub/masters/scoreboard/components/table-row.tsx
+++ apps/web/src/app/sub/masters/scoreboard/components/table-row.tsx
@@ -1,19 +1,20 @@
-import { Table } from "@radix-ui/themes";
-import type { Player } from "@/types";
-import { cn } from "@/utils/ui";
-import { getPlayerName } from "@/utils/utils";
+import {Table, Tooltip} from "@radix-ui/themes";
+import type {Player} from "@/types";
+import {cn} from "@/utils/ui";
+import {getPlayerName} from "@/utils/utils";
+import {IconX} from "@tabler/icons-react";
 
 interface Props {
-	player?: Player;
-	index: number;
+  player?: Player;
+  index: number;
 }
 
-export function TableRow({ player, index }: Props) {
-  if(!player) {
+export function TableRow({player, index}: Props) {
+  if (!player) {
     return null;
   }
 
-	return (
+  return (
       <Table.Row
           className={cn([
             index === 0 && "bg-[#FFD700]",
@@ -24,7 +25,7 @@ export function TableRow({ player, index }: Props) {
         <Table.RowHeaderCell>{index + 1}</Table.RowHeaderCell>
         <Table.Cell className="flex items-center">
           {getPlayerName(player.name)}{" "}
-          {/*{renderHasPlayedAllTournamentsIndicator(player, index)}*/}
+          {displayNotQualifiedMarker(player, index)}
         </Table.Cell>
         <Table.Cell justify="center">
           {prettyNumbers(player.tournament1)}
@@ -48,36 +49,37 @@ export function TableRow({ player, index }: Props) {
 }
 
 function prettyNumbers(number: number | undefined) {
-  if(!number) {
+  if (!number) {
     number = 0;
   }
 
   return number.toFixed(2).toString()
 }
 
-// function renderHasPlayedAllTournamentsIndicator(player: Player, index: number) {
-// 	if (index < 16) {
-// 		return null;
-// 	}
-//
-// 	if (player.hasPlayedAllTournaments || player.name === "Uhl,Lars") {
-// 		return (
-// 			<p>
-// 				<Tooltip
-// 					width="180px"
-// 					content="Für das Finale qualifiziert. Bei allen Turnieren dabei gewesen."
-// 				>
-// 					<IconArrowBadgeUp
-// 						className="text-3xl text-red-600"
-// 						aria-hidden="true"
-// 					/>
-// 				</Tooltip>
-// 				<span className="sr-only">
-// 					Für das Finale qualifiziert. Bei allen Turnieren dabei gewesen.
-// 				</span>
-// 			</p>
-// 		);
-// 	}
-//
-// 	return null;
-// }
+function displayNotQualifiedMarker(player: Player, index: number) {
+  if (index < 16 && player.playedTournaments < 3) {
+    return (
+        <p>
+          <Tooltip
+              width="180px"
+              content={
+                <div>
+                  <p>Nicht für das Finale qualifiziert.</p>
+                  <p>Mindestvoraussetzung sind drei gewertete Turniere.</p>
+                </div>
+              }
+          >
+            <IconX
+                className="text-3xl text-red-600"
+                aria-hidden="true"
+            />
+          </Tooltip>
+
+          <span className="sr-only">
+					   Nicht für das Finale qualifiziert. Mindestvoraussetzung sind drei gewertete Turniere.
+				  </span>
+        </p>
+    );
+  }
+  return null;
+}
